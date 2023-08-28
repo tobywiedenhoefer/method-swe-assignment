858d49e57bef3f3766434442c8ff6b034fb6fe77
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-08-28 14:21:09 -0500

    Set up navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,48 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  MenuItem,
+} from '@mui/material'
+
 const Header = () => {
-    return <></>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <MenuItem key='home' onClick={() => {}}>
+            <Typography 
+              component='a' 
+              variant='h5' 
+              href='/'
+              style={{ textDecoration: 'none', color: 'white' }}
+            >
+              Dunkin Payments
+            </Typography>
+          </MenuItem>
+          <div style={{ marginLeft: 'auto', display: 'flex', 'justifyContent': 'space-between'}}>
+            <Typography 
+              component='a'
+              variant='h6' 
+              href='/payouts'
+              style={{ textDecoration: 'none', color: 'white', paddingRight: '5rem'}}
+            >
+              Payouts
+            </Typography>
+            <Typography 
+              component='a'
+              variant='h6' 
+              href='/reporting'
+              style={{ textDecoration: 'none', color: 'white' }}
+            >
+              Reporting
+            </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
 }
 
 export default Header
