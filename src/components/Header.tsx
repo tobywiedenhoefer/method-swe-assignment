dc4ff0bfe744df21bf37ddb86294b7491c4e7ca2
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-08-28 21:13:28 -0500

    Updating to use useNavigate instead of href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,41 +5,41 @@ import {
   Box,
   MenuItem,
 } from '@mui/material'
-import { Outlet } from 'react-router-dom'
+import FreeBreakfastOutlinedIcon from '@mui/icons-material/FreeBreakfastOutlined';
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const nav = useNavigate()
+  const navigate = {
+    home: () => nav('/'),
+    payouts: () => nav('/payouts'),
+    reporting: () => nav('/reporting')
+  }
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position='static'>
-          <Toolbar>
-            <MenuItem key='home' onClick={() => {}}>
+          <Toolbar className='navbar-anchor'>
+            <MenuItem onClick={navigate.home}>
+              <FreeBreakfastOutlinedIcon />
               <Typography 
                 component='a' 
                 variant='h5' 
-                href='/'
-                style={{ textDecoration: 'none', color: 'white' }}
               >
                 Dunkin Payments
               </Typography>
             </MenuItem>
-            <div style={{ marginLeft: 'auto', display: 'flex', 'justifyContent': 'space-between'}}>
-              <Typography 
-                component='a'
-                variant='h6' 
-                href='/payouts'
-                style={{ textDecoration: 'none', color: 'white', paddingRight: '5rem'}}
-              >
-                Payouts
-              </Typography>
-              <Typography 
-                component='a'
-                variant='h6' 
-                href='/reporting'
-                style={{ textDecoration: 'none', color: 'white' }}
-              >
-                Reporting
-              </Typography>
+            <div className='navbar'>
+              <MenuItem onClick={navigate.payouts}>
+                <Typography variant='h6'>
+                  Payouts
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={navigate.reporting}>
+                <Typography variant='h6'>
+                  Reporting
+                </Typography>
+              </MenuItem>
             </div>
           </Toolbar>
         </AppBar>
