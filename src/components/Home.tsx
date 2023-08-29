521f1a6c120c3386c010235778541439f324bc41
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-08-28 21:12:34 -0500

    Adding basic navigation to root page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@mui/material'
+import { UploadFileSharp, AssessmentSharp } from '@mui/icons-material'
+
 const Home = () => {
-    return <></>
+    const nav = useNavigate()
+    const navigate = {
+        payouts: () => nav('/payouts'),
+        reporting: () => nav('/reporting')
+    }
+    return (
+       <div className='button-container'>
+            <Button size='large' component='label' variant='contained' startIcon={<UploadFileSharp />} onClick={navigate.payouts}>
+                Upload Biweekly Payouts
+            </Button>
+            <Button size='large' color='primary' variant='outlined' startIcon={<AssessmentSharp />} onClick={navigate.reporting}>
+                Access Reporting
+            </Button>
+       </div> 
+    )
 }
 
 export default Home
